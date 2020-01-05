999d11edba8e65f359e047f22c33c2205b0ff5c4
Aset Otinchiev <sharkandshadow@mail.ru>
2020-01-05 15:21:42 +0600

    Add project files.

---
+++ InventoryApiSecond/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InventoryAPI.Models;
+
+namespace InventoryAPI.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Customer GetById(int id);
+        Customer GetByName(string name);
+        int Insert2(Customer customer);
+        int Update2(Customer customer);
+        int Delete2(int id);
+    }
+}
---
+++ InventoryApiSecond/Repositories/CustomerRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using InventoryAPI.Interfaces;
+using InventoryAPI.Models;
+using RepoDb;
+
+namespace InventoryAPI.Repositories
+{
+    public class CustomerRepository : BaseRepository<Customer, SqlConnection>, ICustomerRepository
+    {
+        public CustomerRepository(string connectionString)
+            : base(connectionString)
+        { }
+
+        public Customer GetById(int id)
+        {
+            return Query(id).FirstOrDefault();
+        }
+
+        public Customer GetByName(string name)
+        {
+            return Query(e => e.Name == name).FirstOrDefault();
+        }
+        public int Insert2(Customer customer)
+        {
+            return Insert<int>(customer);
+        }
+
+        public int Update2(Customer customer)
+        {
+            return Update(customer);
+        }
+
+        public int Delete2(int id)
+        {
+            return Delete(id);
+        }
+    }
+}
